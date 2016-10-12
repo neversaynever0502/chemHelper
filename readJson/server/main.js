7c655a580591a2d3830b389e1eb4e3418758e383
neversaynever0502 <neversaynever0502@gmail.com>
2016-10-12 19:32:55 +0800

    fist

---
+++ readJson/server/main.js
@@ -0,0 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(function() {
+  // console.log(JSON.parse(Assets.getText('data.json')));
+});
---
+++ readJson/test/test.js
@@ -0,0 +1,12 @@
+// $.getJSON( "test.json", function( data ) {
+// 			  var items = [];
+// 			  $.each( data, function( key, val ) {
+// 			    items.push( "<li id='" + key + "'>" + val + "</li>" );
+// 			  });
+// 			  console.log(items);
+			 
+// 			  $( "<ul/>", {
+// 			    "class": "my-new-list",
+// 			    html: items.join( "" )
+// 			  }).appendTo( "body" );
+// });
